ac60cc57409a9c6ef3c461baaec06792c8a40766
subhajit2024 <159030694+subhajit2024@users.noreply.github.com>
2024-02-15 09:22:22 +0530

    Add files via upload

---
+++ goal_parser.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string interpret(string command) {
+        int n = command.length();
+        string ans = "";
+        for(int i=0;i<=n;i++)
+        {
+            if(command[i]=='G')
+            {
+                ans+="G";
+            }
+            if(command[i] == '(' && command[i+1] == ')')
+            {
+                ans+="o";
+            }
+            else if(command[i] == '(' && command[i+1] == 'a')
+            {
+                ans+="al";
+            }
+        }
+        return ans;
+    }
+};
